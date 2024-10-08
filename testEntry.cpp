0abc3091530f60365b081c1ab745a6bc5474bb14
a31nesta <anthroponesta@gmail.com>
2024-10-08 08:31:19 +0200

    copy a better exec function from the internet

--- testEntry.cpp
+++ testEntry.cpp
@@ -4,19 +4,23 @@
 
 #include <bgfx/bgfx.h>
 #include <cstdint>
-#include <memory>
-#include <array>
+#include <stdexcept>
 
+// Run command and get output.
+// When the shell is sus
 std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) 
+            result += buffer;
+    } catch (...) {
+        pclose(pipe);
+        throw;
     }
-    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
+    pclose(pipe);
     return result;
 }
 
