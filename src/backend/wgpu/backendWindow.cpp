37b85ae2ff2b69521ac2493c43acf627c66a991b
A31Nesta <anthroponesta@gmail.com>
2024-12-08 18:19:57 +0100

    HELLO TRIANGLE!!!!!!!111!

--- src/backend/wgpu/backendWindow.cpp
+++ src/backend/wgpu/backendWindow.cpp
@@ -39,9 +39,13 @@ namespace pen::backend {
     // ---------------------
     WGPUDevice device;
     WGPUQueue queue;
+    // Surface
     WGPUSurface surface;
     WGPUSurfaceCapabilities surfaceCapabilities;
     WGPUSurfaceConfiguration config;
+    // Pipeline
+    WGPUTextureFormat surfaceFormat = WGPUTextureFormat_Undefined;
+    WGPURenderPipeline pipeline;
 
     // WGPU Helper Variables
     // ---------------------
@@ -70,6 +74,139 @@ namespace pen::backend {
         // wgpuTextureRelease(surfaceTexture.texture);
 
         return targetView;
+    }
+
+    // TODO: Move this to shader code
+    void InitPipeline() {
+        // Create Shader module
+        WGPUShaderModuleDescriptor shaderDesc = {};
+        shaderDesc.hintCount = 0;
+        shaderDesc.hints = nullptr;
+
+        // Shader Code Module Descriptor
+        WGPUShaderModuleWGSLDescriptor shaderCodeDesc = {}; // We use WGSL for shaders. We could use SPIR-V or GLSL though
+        shaderCodeDesc.chain.next = nullptr;
+        shaderCodeDesc.chain.sType = WGPUSType_ShaderModuleWGSLDescriptor;
+        // Connect Chain
+        shaderDesc.nextInChain = &shaderCodeDesc.chain;
+        // Set Shader Source
+        shaderCodeDesc.code = R"(
+            @vertex
+            fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4f {
+                var p = vec2f(0.0, 0.0);
+                if (in_vertex_index == 0u) {
+                    p = vec2f(-0.5, -0.5);
+                } else if (in_vertex_index == 1u) {
+                    p = vec2f(0.5, -0.5);
+                } else {
+                    p = vec2f(0.0, 0.5);
+                }
+                return vec4f(p, 0.0, 1.0);
+            }
+
+            @fragment
+            fn fs_main() -> @location(0) vec4f {
+                return vec4f(1.0, 1.0, 1.0, 1.0);
+            }
+        )";
+
+        WGPUShaderModule shaderModule = wgpuDeviceCreateShaderModule(device, &shaderDesc);
+
+        // Init render pipeline
+        WGPURenderPipelineDescriptor pipelineDesc = {};
+        pipelineDesc.nextInChain = nullptr;
+        // DESCRIBE RENDER PIPELINE
+        // ------------------------
+
+        // Describe vertex pipeline state
+        // ------------------------------
+        pipelineDesc.vertex.bufferCount = 0; // TODO: Enable vertex buffer things amogus sussy
+        pipelineDesc.vertex.buffers = nullptr;
+        // NB: We define the 'shaderModule' in the second part of this chapter.
+        // Here we tell that the programmable vertex shader stage is described
+        // by the function called 'vs_main' in that module.
+        pipelineDesc.vertex.module = shaderModule;
+        pipelineDesc.vertex.entryPoint = "vs_main";
+        pipelineDesc.vertex.constantCount = 0;
+        pipelineDesc.vertex.constants = nullptr;
+
+        // Describe primitive pipeline state
+        // ---------------------------------
+        // Each sequence of 3 vertices is considered as a triangle
+        pipelineDesc.primitive.topology = WGPUPrimitiveTopology_TriangleList;
+        // We'll see later how to specify the order in which vertices should be
+        // connected. When not specified, vertices are considered sequentially.
+        pipelineDesc.primitive.stripIndexFormat = WGPUIndexFormat_Undefined; // TODO: Enable index buffer amogus thing
+        // The face orientation is defined by assuming that when looking
+        // from the front of the face, its corner vertices are enumerated
+        // in the counter-clockwise (CCW) order.
+        pipelineDesc.primitive.frontFace = WGPUFrontFace_CCW;
+        // But the face orientation does not matter much because we do not
+        // cull (i.e. "hide") the faces pointing away from us (which is often
+        // used for optimization).
+        pipelineDesc.primitive.cullMode = WGPUCullMode_None; // TODO: Cull back faces once everything is working (Set to Front)
+
+        // Describe fragment pipeline state
+        // --------------------------------
+        // We tell that the programmable fragment shader stage is described
+        // by the function called 'fs_main' in the shader module.
+        WGPUFragmentState fragmentState = {};
+        fragmentState.module = shaderModule;
+        fragmentState.entryPoint = "fs_main";
+        fragmentState.constantCount = 0;
+        fragmentState.constants = nullptr;
+        
+        // Blending
+        WGPUBlendState blendState = {};
+        // Color blending equation
+        blendState.color.srcFactor = WGPUBlendFactor_SrcAlpha;
+        blendState.color.dstFactor = WGPUBlendFactor_OneMinusDstAlpha;
+        blendState.color.operation = WGPUBlendOperation_Add;
+        // Alpha blending equation
+        blendState.alpha.srcFactor = WGPUBlendFactor_Zero;
+        blendState.alpha.dstFactor = WGPUBlendFactor_One;
+        blendState.alpha.operation = WGPUBlendOperation_Add;
+
+        WGPUColorTargetState colorTarget = {};
+        colorTarget.format = surfaceFormat;
+        colorTarget.blend = &blendState;
+        colorTarget.writeMask = WGPUColorWriteMask_All; // We could write to only some of the color channels.
+       
+        // We have only one target because our render pass has only one output color
+        // attachment.
+        fragmentState.targetCount = 1;
+        fragmentState.targets = &colorTarget;
+
+        // Set fragment
+        pipelineDesc.fragment = &fragmentState;
+
+        // Describe stencil/depth pipeline state
+        // -------------------------------------
+        // Don't use depth for now
+        // TODO: Enable depth for Umbra Renderer Shaders
+        pipelineDesc.depthStencil = nullptr;
+
+        // Describe multi-sampling state
+        // -----------------------------
+        // No multisampling. Sample per pixel = 1
+        // Samples per pixel
+        pipelineDesc.multisample.count = 1;
+        // Default value for the mask, meaning "all bits on"
+        pipelineDesc.multisample.mask = ~0u;
+        // Default value as well (irrelevant for count = 1 anyways)
+        pipelineDesc.multisample.alphaToCoverageEnabled = false;
+
+        // Describe pipeline layout
+        // ------------------------
+        // No memory layout for this pipeline
+        // TODO: Memory layout for pipeline lol
+        pipelineDesc.layout = nullptr;
+
+        // Create render pipeline
+        pipeline = wgpuDeviceCreateRenderPipeline(device, &pipelineDesc);
+
+        // Delete shader module. 1 pipeline per shader so....
+        wgpuShaderModuleRelease(shaderModule);
     }
 
 
@@ -110,11 +247,17 @@ namespace pen::backend {
         renderPassDescriptor.colorAttachmentCount = 1;
         renderPassDescriptor.colorAttachments = &renderPassColorAttachment;
 
-        renderPassDescriptor.depthStencilAttachment = nullptr; // We don't use depth or stencil
+        renderPassDescriptor.depthStencilAttachment = nullptr; // TODO: Enable depth for Umbra Renderer Render Pass
         renderPassDescriptor.timestampWrites = nullptr; // We do not measure performance
 
         // Create Render pass
         WGPURenderPassEncoder renderPass = wgpuCommandEncoderBeginRenderPass(encoder, &renderPassDescriptor);
+
+        // Draw triangles to this render pass
+        // 1. Set Pipeline
+        // 2. Draw triangle
+        wgpuRenderPassEncoderSetPipeline(renderPass, pipeline);
+        wgpuRenderPassEncoderDraw(renderPass, 3, 1, 0, 0);
 
         // Finish render pass
         wgpuRenderPassEncoderEnd(renderPass);
@@ -146,7 +289,7 @@ namespace pen::backend {
     }
 
     // Set Window Size.
-    // Rebuilds framebuffers
+    // Reconfigures the Surface
     void BackendWindow::resize(uint16_t x, uint16_t y) {
         width = x;
         height = y;
@@ -208,7 +351,8 @@ namespace pen::backend {
         // Set surface format to whatever the surface uses
         surfaceCapabilities = {};
         wgpuSurfaceGetCapabilities(surface, adapter, &surfaceCapabilities);
-        config.format = surfaceCapabilities.formats[0];
+        surfaceFormat = surfaceCapabilities.formats[0];
+        config.format = surfaceFormat;
         // And we do not need any particular view format
         config.viewFormatCount = 0;
         config.viewFormats = nullptr;
@@ -261,10 +405,16 @@ namespace pen::backend {
 
         // Release the adapter at the end
         wgpuAdapterRelease(adapter);
+
+        // Init pipeline.
+        // This should be moved to the Shader Class later on
+        InitPipeline();
     }
 
     // Deinitializes Backend
     void BackendWindow::deinitBackend() {
+        // Release pipeline
+        wgpuRenderPipelineRelease(pipeline);
         // Unconfigure the surface
         wgpuSurfaceUnconfigure(surface);
         // Free members
