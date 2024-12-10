8bed1a032ec0801b66f86893ba0d36a77817977d
a31nesta <anthroponesta@gmail.com>
2024-12-10 11:08:17 +0100

    sync with PC TODO: Vertex Layout and Vertex and Index buffers

--- src/backend/wgpu/backendVtxLayout.cpp
+++ src/backend/wgpu/backendVtxLayout.cpp
@@ -1,29 +1,31 @@
 #include "../backendVtxLayout.hpp"
+#include "webgpu.h"
 
+#include <cstdint>
 #include <unordered_map>
+#include <vector>
 
 namespace pen::backend {
-    // Maps BackendAttribute to number of floats necessary
-    // INFO: We use floats only because the "indices" attribute is probably unnecessary idk
-    std::unordered_map<BackendAttribute, uint8_t> attribToNum {
-        {Position,  3},
-        {Normal,    3},
-        {Tangent,   3},
-        {Bitangent, 3},
-        {Color0,    3},
-        {Color1,    3},
-        {Color2,    3},
-        {Color3,    3},
-        {Indices,   1}, // What the hell is this?
-        {Weight,    1},
-        {TexCoord0, 2},
-        {TexCoord1, 2},
-        {TexCoord2, 2},
-        {TexCoord3, 2},
-        {TexCoord4, 2},
-        {TexCoord5, 2},
-        {TexCoord6, 2},
-        {TexCoord7, 2}
+    std::unordered_map<BackendAttribute, uint32_t> penToWGPUShaderLocation {
+        {Position, 0},  // Position
+        {TexCoord0, 1}, // UV Mapping 1
+        {TexCoord1, 2}, // UV Mapping 2 for lights
+        {Normal, 3},    // After UVs we have Normals, Tangents and Bitangents for Normal Maps
+        {Tangent, 4},
+        {Bitangent, 5},
+        // Stuff we probably won't use
+        {Color0, -1},
+        {Color1, -1},
+        {Color2, -1},
+        {Color3, -1},
+        {Indices, -1},
+        {Weight, -1},
+        {TexCoord2, -1},
+        {TexCoord3, -1},
+        {TexCoord4, -1},
+        {TexCoord5, -1},
+        {TexCoord6, -1},
+        {TexCoord7, -1}
     };
 
     // Destructor. If data has something, delete it
@@ -49,7 +51,31 @@ namespace pen::backend {
         }
 
         // If we didn't calculate it before, do it now
-        // Create the Vertex Layout
+        WGPUVertexBufferLayout vertexBufferLayout{};
+        std::vector<WGPUVertexAttribute> attributes;
+        uint16_t offset = 0;
+
+        // Loop
+        for (BackendAttribute attr : vtxLayout) {
+            WGPUVertexFormat format = WGPUVertexFormat_Float32x3; // Defaults to 3 Floats, changes to 2 floats for UVs
+            if (attr == BackendAttribute::TexCoord0 || attr == BackendAttribute::TexCoord1) {
+                format = WGPUVertexFormat_Float32x2;
+            }
+
+            // Create attribute and add it to the list
+            WGPUVertexAttribute vtxAttribute;
+            vtxAttribute.shaderLocation = penToWGPUShaderLocation[attr]; // Set Shader Location
+            vtxAttribute.format = format; // Set Format (2 or 3 Floats)
+            vtxAttribute.offset = offset; // Set Offset. For the first attribute it is 0
+
+            // Add to attribute list
+            attributes.push_back(vtxAttribute);
+
+            // Update Offset
+            offset += (format==WGPUVertexFormat_Float32x3)?(sizeof(float) * 3):(sizeof(float) * 2);
+        }
+        
+        // 
 
         // Save data pointer and return
         // data = (void*) <Whatever the fuck goes here>;
